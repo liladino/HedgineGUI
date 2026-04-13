ad60f6661ceb4263a35db19e751726fac6c42b20
liladino <pampadaniel10@gmail.com>
2026-04-13 07:54:47 +0200

    error causing import removed

--- HedgineGUI/src/game/GameManager.java
+++ HedgineGUI/src/game/GameManager.java
@@ -12,7 +12,6 @@ import core.clock.ClockSnapshot;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.StructuredTaskScope.TimeoutException;
 
 import graphics.dialogs.InformationDialogs;
 import utility.*;
